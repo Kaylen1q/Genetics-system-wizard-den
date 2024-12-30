c48462bd025cbed8b65456241fdd6d63a87dd043
Kaylen1q <iesquirex@gmail.com>
2024-12-30 04:00:38 +0100

    with tabs

--- Content.Client/DNA/UI/DNAConsoleWindow.xaml.cs
+++ Content.Client/DNA/UI/DNAConsoleWindow.xaml.cs
@@ -26,64 +26,14 @@ namespace Content.Client.DNA.UI
             {
                 if (!state.ScannerInRange)
                 {
-                    GeneticScannerFar.Visible = true;
-                    GeneticScannerContents.Visible = false;
-                    GeneticScannerMissing.Visible = false;
-                    return;
+                   
                 }
 
-                GeneticScannerContents.Visible = true;
-                GeneticScannerFar.Visible = false;
-                GeneticScannerMissing.Visible = false;
-                CloneButton.Disabled = state.CloningStatus != ClonerStatus.Ready;
+              
+              
+              
+              
 
-                switch (state.CloningStatus)
-                {
-                    case ClonerStatus.NoClonerDetected:
-                        CloningActivity.Text = (Loc.GetString("cloning-console-component-msg-no-cloner"));
-                        break;
-                    case ClonerStatus.Ready:
-                        CloningActivity.Text = (Loc.GetString("cloning-console-component-msg-ready"));
-                        break;
-                    case ClonerStatus.ClonerOccupied:
-                        CloningActivity.Text = (Loc.GetString("cloning-console-component-msg-occupied"));
-                        break;
-                    case ClonerStatus.ScannerEmpty:
-                        CloningActivity.Text = (Loc.GetString("cloning-console-component-msg-empty"));
-                        break;
-                    case ClonerStatus.ScannerOccupantAlive:
-                        CloningActivity.Text = (Loc.GetString("cloning-console-component-msg-scanner-occupant-alive"));
-                        break;
-                    case ClonerStatus.OccupantMetaphyiscal:
-                        CloningActivity.Text = (Loc.GetString("cloning-console-component-msg-already-alive"));
-                        break;
-                    case ClonerStatus.NoMindDetected:
-                        CloningActivity.Text = (Loc.GetString("cloning-console-component-msg-no-mind"));
-                        break;
-                }
-                // Set label depending on if scanner is occupied or not.
-                ScannerInfoLabel.SetMarkup(state.ScannerBodyInfo != null ?
-                    Loc.GetString("cloning-console-window-scanner-id", ("scannerOccupantName", state.ScannerBodyInfo)) :
-                    Loc.GetString("cloning-console-window-id-blank"));
-            }
-            else
-            {
-                // Scanner is missing, set error message visible
-                GeneticScannerContents.Visible = false;
-                GeneticScannerFar.Visible = false;
-                GeneticScannerMissing.Visible = true;
-            }
-
-            // BUILD ClONER UI
-            if (state.ClonerConnected)
-            {
-                if (!state.ClonerInRange)
-                {
-                    CloningPodFar.Visible = true;
-                    CloningPodContents.Visible = false;
-                    CloningPodMissing.Visible = false;
-                    return;
-                }
 
                 CloningPodContents.Visible = true;
                 CloningPodFar.Visible = false;
